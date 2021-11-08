13539ae82265506e206c670646f9ea4bc24d9d96
drnachio <nacho@binpar.com>
2021-11-08 18:38:13 +0100

    chore: work in progress

---
+++ src/components/layout.tsx
@@ -0,0 +1,22 @@
+import Alert from './alert'
+import Footer from './footer'
+import Meta from './meta'
+
+interface Props {
+  // eslint-disable-next-line react/require-default-props
+  preview?: boolean;
+  children: React.ReactNode;
+}
+
+const Layout = ({ preview, children }: Props) => (
+    <>
+      <Meta />
+      <div className="min-h-screen">
+        <Alert preview={preview} />
+        <main>{children}</main>
+      </div>
+      <Footer />
+    </>
+  )
+
+export default Layout
